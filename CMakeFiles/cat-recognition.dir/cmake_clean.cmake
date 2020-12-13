5bdcb1d1fdb8c5d1b68b70426f1e773431c0920a
Aadithya Prakash <aadithya@murreletinnovation.com>
2020-12-13 14:19:13 -0800

    initial commit with object detection example provided by NVIDIA

---
+++ CMakeFiles/cat-recognition.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cat-recognition.dir/cat-recognition.cpp.o"
+  "cat-recognition.pdb"
+  "cat-recognition"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cat-recognition.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
